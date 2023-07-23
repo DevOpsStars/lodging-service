6a358b51382f623f7b5bcf6f2661491b2e610ad6
isidora-stanic <isidora.stanic@outlook.com>
2023-07-23 14:43:46 +0200

    feat: added postgresql that runs in docker

--- src/test/java/com/devops/lodgingservice/TestLodgingServiceApplication.java
+++ src/test/java/com/devops/lodgingservice/TestLodgingServiceApplication.java
@@ -2,9 +2,18 @@ package com.devops.lodgingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestLodgingServiceApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>("postgres:latest");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.from(LodgingServiceApplication::main).with(TestLodgingServiceApplication.class).run(args);
