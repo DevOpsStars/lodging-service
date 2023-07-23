2369f93ab0da0c46abf56f865f180ac2f1dc3d0e
isidora-stanic <isidora.stanic@outlook.com>
2023-07-23 14:43:46 +0200

    feat: added postgresql that runs in docker

--- src/test/java/com/devops/lodgingservice/LodgingServiceApplicationTests.java
+++ src/test/java/com/devops/lodgingservice/LodgingServiceApplicationTests.java
@@ -2,8 +2,11 @@ package com.devops.lodgingservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
+@SpringBootTest(classes = TestLodgingServiceApplication.class)
 class LodgingServiceApplicationTests {
 
 	@Test
